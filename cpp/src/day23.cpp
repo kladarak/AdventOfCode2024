42db012850152111dded7f96e431277f5866f81b
kladarak <scottjenkins88@gmail.com>
2024-12-20 20:56:26 +0100

    day2X stubs

---
+++ cpp/src/day23.cpp
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <fstream>
+#include <functional>
+#include <map>
+#include <numeric>
+#include <optional>
+#include <ranges>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "String.h"
+#include "Renderer.h"
+#include "Vec2.h"
+
+namespace d23
+{
+	struct Data23
+	{
+	};
+
+	static auto loadData(const char* filename)
+	{
+		Data23 data;
+
+		for (std::string& line : String::readLines(filename))
+		{
+			if (line.empty())
+				continue;
+
+		}
+
+		return data;
+	}
+
+	static uint64_t partOne([[maybe_unused]] const Data23& data)
+	{
+		return 0;
+	}
+
+	static uint64_t partTwo([[maybe_unused]] const Data23& data)
+	{
+		return 0;
+	}
+
+	static void processPrintAndAssert(const char* filename, auto expected1, auto expected2)
+	{
+		const auto data = loadData(filename);
+		const auto result1 = partOne(data);
+		const auto result2 = partTwo(data);
+
+		std::cout << "Part 1: " << result1 << " Part 2: " << result2 << std::endl;
+
+		assert(expected1 == decltype(expected1){} || result1 == expected1);
+		assert(expected2 == decltype(expected2){} || result2 == expected2);
+	}
+}
+
+void day23()
+{
+	d23::processPrintAndAssert("../data/23/test.txt", 0ull, 0ull);
+	d23::processPrintAndAssert("../data/23/real.txt", 0ull, 0ull);
+}
